b1c04b8c7594de59baab7b84dc7e65faa08822b2
陳灝 <howardchen@Howard-Chen-de-MacBook-Pro.local>
2020-08-07 11:28:51 +0800

    update

--- checkpwd.php
+++ checkpwd.php
@@ -8,11 +8,8 @@
     $row = mysqli_fetch_array($result, MYSQLI_NUM);
 
 if($_POST['password']==$row[1]){
-    $id = mysqli_fetch_object($result)->id;
     mysqli_free_result($result);
     mysqli_close($link);
-    
-    
     $_SESSION['account'] = $_POST['account'];
     header("location:派發系統.php");  
     exit();
